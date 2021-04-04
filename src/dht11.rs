8851e3e549b75f71025a592fb9f0fcb18d129c7d
Morgan Roff <roffmorgan@gmail.com>
2021-04-04 14:57:58 +0200

    Remove std_library and add more tests

--- src/dht11.rs
+++ src/dht11.rs
@@ -1,189 +1,182 @@
 use embedded_hal::digital::{InputPin, OutputPin, IoPin, PinState};
-// TODO: Remove the std library for true portability.
-use std::fmt;
-use std::time::{Instant, Duration};
-use tokio::time::{sleep, sleep_until};
+use core::time::Duration;
 
 #[derive(Debug)]
 pub enum Error<TIoError> {
     /// Wrapped error from the HAL.
     Wrapped(TIoError),
     /// Invalid argument was provided.
-    InvalidArgument(String),
+    InvalidArgument(),
     /// Invalid data was read.
-    Data(String),
+    BadParity(),
+    /// Timeout occurred.
+    Timeout(),
 }
 
-impl<TIoError> std::error::Error for Error<TIoError> 
-    where TIoError : fmt::Debug + std::error::Error + 'static {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            &Error::Wrapped(ref err) => Some(err),
-            _ => None,
-        }
+impl<TIoError> From<TIoError> for Error<TIoError> {
+    fn from(error: TIoError) -> Error<TIoError> {
+        Error::Wrapped(error)
     }
 }
 
-impl<TIoError> std::fmt::Display for Error<TIoError>
-    where TIoError : fmt::Debug {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            &Error::Wrapped(ref err) => write!(f, "IO error: {:?}", err),
-            &Error::InvalidArgument(ref msg) => write!(f, "Invalid argument: {}", msg),
-            &Error::Data(ref msg) => write!(f, "Bad data read: {}", msg),
-        }
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct DhtResponse {
-    pub humidity: f32,
-    pub temperature: f32,
+    pub humidity: u8,
+    pub humidity_decimal: u8,
+    pub temperature: i8,
+    pub temperature_decimal: u8,
 }
 
 impl DhtResponse {
     fn from_raw_bytes(bytes: &[u8; 4]) -> DhtResponse {
         DhtResponse {
-            humidity: format!("{}.{}", bytes[0], bytes[1]).parse().unwrap(),
-            temperature: format!("{}.{}", bytes[2] as i8, bytes[3]).parse().unwrap(),
+            humidity: bytes[0],
+            humidity_decimal: bytes[1],
+            temperature: bytes[2] as i8,
+            temperature_decimal: bytes[3],
         }
     }
 }
 
-impl std::fmt::Display for DhtResponse {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "RH: {:.2}%, T: {:.2}\u{00B0}C", self.humidity, self.temperature)
-    }
-}
-
-pub struct Dht11<TInputPin, TOutputPin> {
+pub struct Dht11<TInputPin, TOutputPin, TimeFn, ElapsedFn, TTime> 
+where TimeFn: Fn() -> TTime,
+      ElapsedFn: Fn(TTime) -> Duration,
+      TTime: Copy {
     input_pin: Option<TInputPin>,
     output_pin: Option<TOutputPin>,
-    last_read_time: Instant,
+    minimum_read_interval: Duration,
+    last_read_time: TTime,
+    time_fn: TimeFn,
+    elapsed_since_fn: ElapsedFn,
 }
 
-const MINIMUM_READ_INTERVAL: Duration = Duration::from_secs(1);
+pub const MINIMUM_READ_INTERVAL: Duration = Duration::from_millis(1000);
 
-impl<TInputPin, TOutputPin, TError> Dht11<TInputPin, TOutputPin>
+impl<TInputPin, TOutputPin, TError, TimeFn, ElapsedFn, TTime> Dht11<TInputPin, TOutputPin, TimeFn, ElapsedFn, TTime>
 where TInputPin : InputPin<Error = TError> + IoPin<TInputPin, TOutputPin, Error = TError>,
-      TOutputPin : OutputPin<Error = TError> + IoPin<TInputPin, TOutputPin, Error = TError> {
+      TOutputPin : OutputPin<Error = TError> + IoPin<TInputPin, TOutputPin, Error = TError>,
+      TimeFn: Fn() -> TTime,
+      ElapsedFn: Fn(TTime) -> Duration,
+      TTime: Copy {
 
-    /// Constructs a DHT sensor that reads from the given pin.
-    pub fn new<TIoPin>(pin: TIoPin) -> Result<Dht11<TInputPin, TOutputPin>, Error<TError>> 
-    where TIoPin : IoPin<TInputPin, TOutputPin, Error = TError> {
+    /// Constructs a DHT sensor that reads from the given pin and uses the
+    /// default minimum read interval (1 second).
+    /// 
+    /// Reads can sometimes be more reliable with a longer delay, eg. 2 seconds,
+    /// so consider calling
+    /// [`set_minimum_read_interval`](method@crate::dht11::Dht11::set_minimum_read_interval)
+    /// with a longer interval if error rates are high.
+    ///
+    /// The provided `time_fn` closure should provide some representation of a
+    /// given instant that can be used with `elapsed_sinc_fn` to determine how
+    /// much time has passed since then. It does not need to reflect real
+    /// dates and times, but only needs to be capable of providing reasonably
+    /// accurate durations (i.e. with millisecond precision or better).
+    pub fn new<TIoPin>(pin: TIoPin, time_fn: TimeFn, elapsed_since_fn: ElapsedFn)
+            -> Result<Dht11<TInputPin, TOutputPin, TimeFn, ElapsedFn, TTime>, Error<TError>> 
+            where TIoPin : IoPin<TInputPin, TOutputPin, Error = TError> {
         Ok(Dht11 {
             input_pin: None,
-            output_pin: Some(pin.try_switch_to_output_pin(PinState::High).map_err(|err| Error::Wrapped(err))?),
-            last_read_time: Instant::now(),
+            output_pin: Some(pin.try_switch_to_output_pin(PinState::High)?),
+            minimum_read_interval: MINIMUM_READ_INTERVAL,
+            last_read_time: time_fn(),
+            time_fn: time_fn,
+            elapsed_since_fn: elapsed_since_fn,
         })
+    }
+
+    /// Modifies the minimum read interval enforced by this sensor. This must
+    /// be greater than the DHT's default minimum read interval of 1 second.
+    pub fn set_minimum_read_interval(&mut self, read_interval: Duration) -> Result<(), Error<TError>> {
+        if read_interval < MINIMUM_READ_INTERVAL {
+            return Err(Error::InvalidArgument());
+        }
+
+        self.minimum_read_interval = read_interval;
+        Ok(())
     }
 
     /// Reads data from the DHT sensor using the minimum read interval. 
     /// 
-    /// This will asynchronously sleep if it is called within the minimum read 
-    /// interval of the DHT sensor (1 second). Reads tend to be more reliable
-    /// with a slightly longer delay, eg. 2 seconds, so consider calling
-    /// [`read_with_minimum_interval`](method@crate::dht11::Dht11::read_with_minimum_interval)
-    /// if error rates are high.
+    /// This will asynchronously sleep using the provided `delay_fn` if `read`
+    /// is called within the minimum read interval of this DHT sensor. The
+    /// provided function needs to be capable of millisecond precision or
+    /// better.
     /// 
     /// Due to the tight timing necessary to distinguish bits in the DHT's
     /// response, this performs blocking I/O reads while receiving data. This
     /// takes about 4ms (full range: 3200-4800us, depending on the data).
-    #[allow(dead_code)]
-    pub async fn read(&mut self) -> Result<DhtResponse, Error<TError>> {
-        self.read_with_minimum_interval(MINIMUM_READ_INTERVAL).await
-    }
-
-    /// Reads data from the DHT sensor, enforcing that the given minimum time
-    /// interval has passed between reads.
-    /// 
-    /// This will asynchronously sleep if it is called within the provided
-    /// minimum read interval. This interval must be greater than or equal to
-    /// the minimum DHT sensor read interval (1 second). Reads tend to be more
-    /// reliable with a slightly longer delay, eg. 2 seconds, so this method
-    /// gives users greater flexibility if error rates are high.
-    /// 
-    /// Due to the tight timing necessary to distinguish bits in the DHT's
-    /// response, this performs blocking I/O reads while receiving data. This
-    /// takes about 4ms (full range: 3200-4800us, depending on the data).
-    pub async fn read_with_minimum_interval(&mut self, minimum_read_interval: Duration) -> Result<DhtResponse, Error<TError>> {
-        if minimum_read_interval < MINIMUM_READ_INTERVAL {
-            return Err(
-                Error::InvalidArgument(
-                format!("Minimum read interval must be >= {:.1}s.", MINIMUM_READ_INTERVAL.as_secs())));
-        }
-
-        // Double check that the output is driven high so the DHT is reading data.
+    pub async fn read<DelayFn, EmptyFuture>(&mut self, delay_fn: DelayFn) -> Result<DhtResponse, Error<TError>> 
+    where DelayFn: Fn(Duration) -> EmptyFuture,
+          EmptyFuture: core::future::Future<Output = ()> {
+        // Double check that the output is driven high so the DHT is ready to send data.
         if self.output_pin.is_none() {
             self.swap_to_output_mode()?;
         }
 
-        let first_read_time = self.last_read_time + minimum_read_interval;
-        if Instant::now() < first_read_time {
-            sleep_until(first_read_time.into()).await;
+        let elapsed_since_last_read = (self.elapsed_since_fn)(self.last_read_time);
+        if elapsed_since_last_read < self.minimum_read_interval {
+            let to_wait = self.minimum_read_interval - elapsed_since_last_read;
+            delay_fn(to_wait).await;
         }
 
-        self.request_data().await?;
+        self.request_data(delay_fn).await?;
         let bytes = self.receive_data()?;
         Ok(DhtResponse::from_raw_bytes(&bytes))
     }
 
-    async fn request_data(&mut self) -> Result<(), Error<TError>> {
+    async fn request_data<DelayFn, EmptyFuture>(&mut self, delay_fn: DelayFn) -> Result<(), Error<TError>> 
+    where DelayFn: Fn(Duration) -> EmptyFuture,
+          EmptyFuture: core::future::Future<Output = ()> {
         self.output_pin.as_mut().unwrap().try_set_low().map_err(|err| Error::Wrapped(err))?;
-        sleep(Duration::from_millis(18)).await;
+        delay_fn(Duration::from_millis(18)).await;
         Ok(())
     }
 
     fn receive_data(&mut self) -> Result<[u8; 4], Error<TError>> {
-        let mut bit_durations = [Duration::new(0, 0); 40];
+        let mut bit_ticks = [0u32; 40];
         self.input_pin = Some(self.output_pin.take().unwrap().try_switch_to_input_pin().map_err(|err| Error::Wrapped(err))?);
         let input_pin: &TInputPin = &mut self.input_pin.as_ref().unwrap();
 
         // Block for the ACK, and use this to estimate a timeout.
-        let mut counter = match read_ack(input_pin) {
+        let ack_counter = match read_ack(input_pin) {
             Err(err) => {
                 self.swap_to_output_mode()?;
                 return Err(err);
             },
             Ok(count) => count
         };
-        let counter_timeout = counter << 6;
+        let bit_timeout = ack_counter << 2;
 
-        let mut end_of_bit = Instant::now();
-        let mut start_of_bit: Instant;
-        counter = 0;
         for i in 0..40 {
-            counter = match read_bit_with_timeout(input_pin, counter_timeout, counter) {
+            bit_ticks[i] = match read_bit_with_timeout(input_pin, bit_timeout) {
                 Err(err) => {
                     self.swap_to_output_mode()?;
                     return Err(err);
                 }
                 Ok(count) => count
             };
-
-            start_of_bit = end_of_bit;
-            end_of_bit = Instant::now();
-            bit_durations[i] = end_of_bit - start_of_bit;
         }
 
         self.swap_to_output_mode()?;
 
-        let high_humidity = parse_byte(&bit_durations[0..8]);
+        let threshold = determine_tick_threshold(&bit_ticks);
+        let high_humidity = parse_byte(&bit_ticks[0..8], threshold);
         // This DHT11 sensor always sends 0 for this byte. Use hard-coded zero
         // to reduce timing errors.
         let low_humidity = 0;
-        let high_temp = parse_byte(&bit_durations[16..24]);
+        let high_temp = parse_byte(&bit_ticks[16..24], threshold);
         // This DHT11 sensor always sends a value from 0-9 for this byte. Mask
         // the value to reduce timing errors.
-        let low_temp = parse_byte(&bit_durations[24..32]) & 0b1111u8;
-        let parity = parse_byte(&bit_durations[32..40]);
+        let low_temp = parse_byte(&bit_ticks[24..32], threshold) & 0b1111u8;
+        let parity = parse_byte(&bit_ticks[32..40], threshold);
 
         let sum: u16 = high_humidity as u16 + low_humidity as u16 + high_temp as u16 + low_temp as u16;
         // The last 8 bits should match the parity byte.
         let expected_parity = sum.to_be_bytes()[1];
 
         if parity != expected_parity {
-            return Err(Error::Data("Parity mismatch; try again.".to_owned()));
+            return Err(Error::BadParity());
         }
 
         Ok([high_humidity, low_humidity, high_temp, low_temp])
@@ -194,27 +187,26 @@ where TInputPin : InputPin<Error = TError> + IoPin<TInputPin, TOutputPin, Error 
             self.input_pin.take().unwrap()
                 .try_switch_to_output_pin(PinState::High)
                 .map_err(|err| Error::Wrapped(err))?);
-        self.last_read_time = Instant::now();
+        self.last_read_time = (self.time_fn)();
         Ok(())
     }
 }
 
 #[inline]
 fn read_bit_with_timeout<TInput, TError>(
-    input_pin: &TInput, timeout: u32, start_count: u32) -> Result<u32, Error<TError>>
+    input_pin: &TInput, timeout: u32) -> Result<u32, Error<TError>>
 where TInput: InputPin<Error = TError> {
-    const TIMEOUT_MESSAGE: &str = "Timeout while reading data; try again.";
-    let mut counter = start_count;
+    let mut counter = 0u32;
     while input_pin.try_is_low().map_err(|err| Error::Wrapped(err))? {
         counter += 1;
         if counter > timeout {
-            return Err(Error::Data(TIMEOUT_MESSAGE.to_owned()));
+            return Err(Error::Timeout());
         }
     };
     while input_pin.try_is_high().map_err(|err| Error::Wrapped(err))? {
         counter += 1;
         if counter > timeout {
-            return Err(Error::Data(TIMEOUT_MESSAGE.to_owned()));
+            return Err(Error::Timeout());
         }
     };
     Ok(counter)
@@ -230,11 +222,95 @@ where TInput: InputPin<Error = TError> {
     Ok(counter)
 }
 
-fn parse_byte(bit_durations: &[Duration]) -> u8 {
-    const THRESHOLD: Duration = Duration::from_micros(100u64);
+// (index, count) 
+#[derive(Clone, Copy)]
+struct Peak(i8, u8);
+
+fn determine_tick_threshold(bit_ticks: &[u32]) -> u32 {
+    let mut min = u32::MAX;
+    let mut max = 0;
+
+    for ticks in bit_ticks.iter() {
+        if *ticks < min {
+            min = *ticks;
+        }
+        if *ticks > max {
+            max = *ticks;
+        }
+    }
+
+    // Make a histogram.
+    let range = max - min + 1;
+    let mut buckets = [0u8; 10];
+    for ticks in bit_ticks.iter() {
+        let mut bucket = ((*ticks - min) * buckets.len() as u32 / range) as usize;
+        if bucket >= buckets.len() {
+            bucket = buckets.len() - 1;
+        }
+        buckets[bucket] += 1;
+    }
+
+    // Find peaks in the histogram.
+    let mut peaks = [Peak(-1, 0); 10];
+    let mut previous_count = 0;
+    let mut num_peaks = 0;
+    for i in 0..buckets.len() {
+        let count = buckets[i];
+        if count > previous_count {
+            if i == buckets.len() - 1 || count > buckets[i + 1] {
+                peaks[num_peaks] = Peak(i as i8, count);
+                num_peaks += 1;
+            }
+        }
+        previous_count = count;
+    }
+
+    // Take the midpoint if two peaks were not found.
+    if num_peaks < 2 {
+        return min + range / 2;
+    }
+
+    // Determine the two highest peaks. These should correspond to the expected
+    // number of ticks for the zero (short) and one (long) bits.
+    let mut highest_peak = &peaks[peaks.len() - 1];
+    let mut second_highest_peak = &peaks[peaks.len() - 1];
+    for peak in peaks.iter() {
+        let peak_count = peak.1;
+        if peak_count > highest_peak.1 {
+            second_highest_peak = highest_peak;
+            highest_peak = peak;
+        } else if peak_count > second_highest_peak.1 {
+            second_highest_peak = peak;
+        }
+    }
+
+    // Convert a histogram index into an approximate number of ticks.
+    let index_to_ticks = |index| {
+        if index == (buckets.len() - 1) as i8 {
+            return max;
+        }
+        let base = range * index as u32 / 10 + min;
+        let next_base = range * (index as u32 + 1) / 10 + min;
+        return (base + next_base) / 2;
+    };
+
+    let high_ticks: u32;
+    let low_ticks: u32;
+    if highest_peak.0 > second_highest_peak.0 {
+        high_ticks = index_to_ticks(highest_peak.0);
+        low_ticks = index_to_ticks(second_highest_peak.0);
+    } else {
+        low_ticks = index_to_ticks(highest_peak.0);
+        high_ticks = index_to_ticks(second_highest_peak.0);
+    }
+    // Use the mean of the two peaks as the threshold.
+    return (high_ticks + low_ticks) / 2;
+}
+
+fn parse_byte(bit_ticks: &[u32], threshold: u32) -> u8 {
     let mut byte = 0u8;
     for i in 0..8 {
-        if bit_durations[i] > THRESHOLD {
+        if bit_ticks[i] > threshold {
             byte |= 1 << (7 - i);
         }
     }
--- tests/dht11_test.rs
+++ tests/dht11_test.rs
@@ -1,21 +1,62 @@
 use embedded_hal::digital::{InputPin, IoPin, OutputPin, PinState};
-use sensors::dht11;
-use std::time::Duration;
+use common_sensors::dht11;
+use std::time::{Instant, Duration};
 
 #[derive(Debug)]
 enum FakeError {}
 
-struct FakePin {}
+static mut data_index: usize = 0;
+
+struct FakePin {
+    data_to_read: Option<Vec<u8>>,
+}
+
+impl FakePin {
+    fn new() -> FakePin {
+        unsafe {
+            data_index = 0;
+        }
+        FakePin{ data_to_read: None }
+    }
+
+    fn set_data(&mut self, data: Vec<u8>) {
+        self.data_to_read = Some(data);
+        unsafe {
+            data_index = 0;
+        }
+    }
+
+    fn clear_data(&mut self) {
+        self.data_to_read = None;
+        unsafe {
+            data_index = 0;
+        }
+    }
+}
 
 impl InputPin for FakePin {
     type Error = FakeError;
 
     fn try_is_high(&self) -> Result<bool, Self::Error> {
-        return Ok(true);
+        if self.data_to_read.is_none() {
+            return Ok(false);
+        }
+
+        unsafe {
+            data_index += 1;
+            Ok(self.data_to_read.as_ref().unwrap()[data_index - 1] > 0)
+        }
     }
 
     fn try_is_low(&self) -> Result<bool, Self::Error> {
-        return Ok(false);
+        if self.data_to_read.is_none() {
+            return Ok(false);
+        }
+
+        unsafe {
+            data_index += 1;
+            Ok(self.data_to_read.as_ref().unwrap()[data_index - 1] == 0)
+        }
     }
 }
 
@@ -44,9 +85,134 @@ impl IoPin<FakePin, FakePin> for FakePin {
 }
 
 #[tokio::test]
-async fn test_read_with_bad_interval() -> Result<(), dht11::Error<FakeError>> {
-    let mut sensor = dht11::Dht11::new(FakePin{})?;
+async fn test_set_invalid_interval() -> Result<(), dht11::Error<FakeError>> {
+    let mut sensor = dht11::Dht11::new(
+        FakePin::new(),
+        || Instant::now(),
+        |instant| instant.elapsed())?;
 
-    assert!(!sensor.read_with_minimum_interval(Duration::from_nanos(1)).await.is_ok());
+    assert!(!sensor.set_minimum_read_interval(Duration::from_nanos(1)).is_ok());
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_read_all_zeros() -> Result<(), dht11::Error<FakeError>> {
+    let mut sensor = dht11::Dht11::new(
+        FakePin::new(),
+        || Instant::now(),
+        |instant| instant.elapsed())?;
+
+    let result = sensor.read(|duration| tokio::time::sleep(duration.into())).await?;
+    assert_eq!(result, dht11::DhtResponse{ humidity: 0, humidity_decimal: 0, temperature: 0, temperature_decimal: 0});
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_read_with_valid_data() -> Result<(), dht11::Error<FakeError>> {
+    let mut fake_pin = FakePin::new();
+    fake_pin.set_data(vec![
+        /* ACK */
+        1,1,0,0,1,1,
+        /* Byte 0 = 0x11 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,
+        /* Byte 1 = 0x00 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* Byte 2 = 0x0F */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1,1, 0,0,1,1,1, 0,0,1,1,1,
+        /* Byte 3 = 0x00 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* Parity = 0x20 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* End */
+        0,0,1,1,]);
+    let mut sensor = dht11::Dht11::new(
+        fake_pin,
+        || Instant::now(),
+        |instant| instant.elapsed())?;
+
+    let result = sensor.read(|duration| tokio::time::sleep(duration.into())).await?;
+    assert_eq!(result, dht11::DhtResponse{ humidity: 0x11, humidity_decimal: 0, temperature: 0x0F, temperature_decimal: 0});
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_read_bad_parity() -> Result<(), dht11::Error<FakeError>> {
+    let mut fake_pin = FakePin::new();
+    fake_pin.set_data(vec![
+        /* ACK */
+        1,1,0,0,1,1,
+        /* Byte 0 = 0x11 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,
+        /* Byte 1 = 0x00 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* Byte 2 = 0x0F */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1,1, 0,0,1,1,1, 0,0,1,1,1,
+        /* Byte 3 = 0x00 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* Parity = 0x21 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,
+        /* End */
+        0,0,1,1,]);
+    let mut sensor = dht11::Dht11::new(
+        fake_pin,
+        || Instant::now(),
+        |instant| instant.elapsed())?;
+
+    let result = sensor.read(|duration| tokio::time::sleep(duration.into())).await;
+    assert!(match result {
+        Err(dht11::Error::BadParity()) => true,
+        _ => false
+    });
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_read_with_imperfect_timing() -> Result<(), dht11::Error<FakeError>> {
+    let mut fake_pin = FakePin::new();
+    fake_pin.set_data(vec![
+        /* ACK */
+        1,1,0,0,1,1,
+        /* Byte 0 = 0x11 */
+        0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,
+        /* Byte 1 = 0x00 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* Byte 2 = 0x0F */
+        0,0,1,1, 0,0,1, 0,0,1,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,1, 0,0,1,1,1,1,1,1,1,1, 0,0,1,1,1,1,1,1,1,1, 0,0,1,1,1,1,1,1,1,1,1,1,
+        /* Byte 3 = 0x00 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,1, 0,0,1,1,1,
+        /* Parity = 0x20 */
+        0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,
+        /* End */
+        0,0,1,1,]);
+    let mut sensor = dht11::Dht11::new(
+        fake_pin,
+        || Instant::now(),
+        |instant| instant.elapsed())?;
+
+    let result = sensor.read(|duration| tokio::time::sleep(duration.into())).await?;
+    assert_eq!(result, dht11::DhtResponse{ humidity: 0x11, humidity_decimal: 0, temperature: 0x0F, temperature_decimal: 0});
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_read_with_timeout() -> Result<(), dht11::Error<FakeError>> {
+    let mut fake_pin = FakePin::new();
+    fake_pin.set_data(vec![
+        /* ACK */
+        1,1,0,0,1,1,
+        /* Byte 0 = 0x11 */
+        0,0,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,
+        /* Byte 1 = Invalid */
+        0,0,1,1, 0,0,1,1, 0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 ]);
+    let mut sensor = dht11::Dht11::new(
+        fake_pin,
+        || Instant::now(),
+        |instant| instant.elapsed())?;
+
+    let result = sensor.read(|duration| tokio::time::sleep(duration.into())).await;
+    assert!(match result {
+        Err(dht11::Error::Timeout()) => true,
+        _ => false,
+    });
     Ok(())
 }
